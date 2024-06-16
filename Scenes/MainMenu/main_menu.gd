634c9102a0d56caaad018850a0ed6c334522ba0e
Ari Barzilai <aribarzilai@gmail.com>
2024-06-16 11:28:15 +0300

    added menu keyboard controls

--- Scenes/MainMenu/main_menu.gd
+++ Scenes/MainMenu/main_menu.gd
@@ -4,3 +4,12 @@ extends Control
 func _input(event):
 	if event.is_action("ui_reload_game"):
 		get_tree().change_scene_to_file('res://Scenes/SplashScreen/splash_screen.tscn')
+	if event.is_action_pressed("ui_up"):
+		if get_viewport().gui_get_focus_owner() == null:
+			$MarginContainer/Panel/VBoxContainer/SettingsButton.grab_focus()
+		print("UI SELECT: " + get_viewport().gui_get_focus_owner().get_focus_neighbor(SIDE_TOP).get_name(1))
+	elif event.is_action_pressed("ui_down"):
+		if get_viewport().gui_get_focus_owner() == null:
+			$MarginContainer/Panel/VBoxContainer/SettingsButton.grab_focus()
+		print("UI SELECT: " + get_viewport().gui_get_focus_owner().get_focus_neighbor(SIDE_BOTTOM).get_name(1))
+		
